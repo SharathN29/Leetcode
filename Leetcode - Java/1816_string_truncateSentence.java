4125159e185ba8723a5a74b1ffffd1ee941f8a20
Sharath Nagendra <nsharathn@gmail.com>
2021-05-07 16:39:05 -0700

    Problem 1816 - Truncate Sentence

---
+++ Leetcode - Java/1816_string_truncateSentence.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/truncate-sentence/
+
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < k; i++) {
+            sb.append(words[i]);
+            sb.append(' ');
+        }
+        
+        sb.deleteCharAt(sb.length() - 1);
+        
+        return sb.toString();
+    }
+}
